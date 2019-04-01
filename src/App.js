f93f17d037a1603a066a6e1861e07d0e3b957839
ITAI BIGIAOUI <ITAI2700@ITAIs-MacBook-Pro.local>
2019-04-01 17:50:26 +0300

    itayyyyy

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+
+class App extends Component{
+    render(){
+        return (
+            <div>
+                <h1>this is our special secret airbnb project</h1>
+            </div>
+        )
+    }
+}
+
+export default App
+
